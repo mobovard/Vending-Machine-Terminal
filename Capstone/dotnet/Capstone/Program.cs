49b49fdfcecd4146f251112798a63731943995dd
maddy bovard <m.bovard55@gmail.com>
2021-02-05 16:18:30 -0500

    saving changes

--- Capstone/dotnet/Capstone/Program.cs
+++ Capstone/dotnet/Capstone/Program.cs
@@ -8,90 +8,14 @@ namespace Capstone
     class Program
     {
         static void Main(string[] args)
-<<<<<<< HEAD
         {
-            //use filePath to create a dictionary<slot, Food>
-            string filePath = @"C:\Users\Student\workspace\module1-capstone-c-team-4\Example Files\Inventory.txt";
-            Dictionary<string, Food> foodDictionary = VendingMachine.Stock(filePath);
-=======
-        {         
             string userInput = "";
->>>>>>> 6a19437ecf8970e1d154f6f47cc7e6357bc68361
 
             while (userInput != "3")
             {
                 //use filePath to create a dictionary<slot, Food>
-                string filePath = @"C:\Users\Student\workspace\TestCapstone\dont_push-module1-capstone-c-team-4\Capstone\dotnet\Capstone\Files\Inventory.txt";
+                string filePath = @"C:\Users\Student\workspace\module1-capstone-c-team-4\Example Files\Inventory.txt";
 
-<<<<<<< HEAD
-            string userInput = "";
-
-            while (userInput != "3")
-            {
-                //prompt the user
-                VendingMachine.DisplayPromptToUser();
-
-                //capture user response
-                userInput = Console.ReadLine();
-                Console.WriteLine();
-                Console.Clear();
-
-                switch (userInput)
-                {
-                    case "1":
-                        {
-                            while (userInput != "e")
-                            {
-                                Console.Clear();
-                                //Display vending machine items
-                                VendingMachine.DisplayItems(foodDictionary);
-                                Console.WriteLine();
-
-                                //Allow user to exit and return to main prompt
-                                Console.Write("Press e to exit: ");
-                                userInput = Console.ReadLine();
-                                Console.Clear();
-                            }
-
-                            break;
-                        }
-
-                    case "2":
-                        {
-                            while (userInput != "e")
-                            {
-                                //PURCHASE SCREEN
-                                Console.WriteLine("(1) Feed Money");
-                                Console.WriteLine("(2) Select Product");
-                                Console.WriteLine("(3) Finish Transaction");
-                                Console.WriteLine($"Current Money Provided: {currentUser.Balance}\n");
-
-                                string purchaseScreenInput = Console.ReadLine();
-                                Console.WriteLine();
-
-                                //FEED MONEY
-                                if (purchaseScreenInput == "1")
-                                {
-                                    Console.Clear();
-                                    //user can add money to their balance
-                                    Console.Write("Please add a whole dollar amount to your balance: ");
-                                    decimal currentBalance = currentUser.Balance += Convert.ToDecimal(Console.ReadLine());
-                                    Console.Clear();
-                                }
-
-                                //SELECT PRODUCT
-                                while (purchaseScreenInput == "2")
-                                {
-                                    //Display items to user
-                                    Console.Clear();
-                                    VendingMachine.DisplayItems(foodDictionary);
-                                    Console.WriteLine();
-
-                                    //prompt user to select an item
-                                    Console.Write("Please enter the slot code of the item you want: ");
-                                    string userSelection = Console.ReadLine();
-
-=======
                 Dictionary<string, Food> foodDictionary = VendingMachine.Stock(filePath);
 
                 //instantiate a user that has a Balance property
@@ -164,7 +88,6 @@ namespace Capstone
                                     Console.Write("Please enter the slot code of the item you want: ");
                                     string userSelection = Console.ReadLine();
 
->>>>>>> 6a19437ecf8970e1d154f6f47cc7e6357bc68361
                                     //check that the user selection is in the vending machine
                                     if (foodDictionary.ContainsKey(userSelection))
                                     {
@@ -207,7 +130,6 @@ namespace Capstone
                                                 }
 
                                             }
-<<<<<<< HEAD
 
                                         }
                                     }
@@ -217,61 +139,9 @@ namespace Capstone
                                         Console.WriteLine("Sorry, item not found...\n");
                                         break;
                                     }
-                                    break;
-                                }
-
-                                // FINISH TRANSACTION
-                                while (purchaseScreenInput == "3")
-                                {
-                                    decimal balance = currentUser.Balance;
-                                    string balanceString = balance.ToString("0.00");
-                                    balance = Convert.ToDecimal(balanceString);
-                                    balance *= 100;
-
-                                    int numberOfQuarters = 0;
-                                    int numberOfDimes = 0;
-                                    int numberOfNickels = 0;
-
-                                    while (balance > 25)
-                                    {
-                                        numberOfQuarters++;
-                                        balance -= 25;
-=======
-
-                                        }
-                                    }
-                                    else
-                                    {
-                                        Console.Clear();
-                                        Console.WriteLine("Sorry, item not found...\n");
-                                        break;
->>>>>>> 6a19437ecf8970e1d154f6f47cc7e6357bc68361
-                                    }
-                                    while (balance > 10)
-                                    {
-                                        numberOfDimes++;
-                                        balance -= 10;
-                                    }
-                                    while (balance > 0)
-                                    {
-                                        numberOfNickels++;
-                                        balance -= 5;
-                                    }
-
-                                    Console.Clear();
-                                    currentUser.Balance = balance;
-                                    Console.WriteLine($"Here is your change, you have {numberOfQuarters} quarters, {numberOfDimes} dimes and {numberOfNickels} nickels");
-                                    userInput = "3";
-                                    break;
 
                                     break;
                                 }
-<<<<<<< HEAD
-
-                            }
-
-                            break;
-=======
                                 //FINISH TRANSACTION
                                 while (purchaseScreenInput == "3")
                                 {
@@ -281,7 +151,6 @@ namespace Capstone
 
                                     //Make balance a whole number
                                     balance *= 100;
->>>>>>> 6a19437ecf8970e1d154f6f47cc7e6357bc68361
 
                                     double numberOfQuarters = 0;
                                     double numberOfDimes = 0;
@@ -293,10 +162,6 @@ namespace Capstone
                                         numberOfQuarters++;
                                     }
 
-<<<<<<< HEAD
-
-                }
-=======
                                     while (balance > 10)
                                     {
                                         balance -= 10;
@@ -313,7 +178,6 @@ namespace Capstone
                                     currentUser.Balance = balance;
 
                                     Console.WriteLine($"Here's your change: {numberOfQuarters} Quarter(s), {numberOfDimes} Dime(s), and {numberOfNickels} Nickel(s)");
->>>>>>> 6a19437ecf8970e1d154f6f47cc7e6357bc68361
 
                                     userInput = "3";
                                     break;
